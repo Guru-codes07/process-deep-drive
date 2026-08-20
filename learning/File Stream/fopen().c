a2386e5a1779bae603808ecf188adfe8e4dec262
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-20 17:27:11 +0530

    Implement file opening and writing with error check
    Fix fopen error handling and correct fprint typo.

--- learning/File Stream/fopen().c
+++ learning/File Stream/fopen().c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  FILE *fp = fopen("hehe.txt","w");
+  if(fp == NULL)
+  {
+    perror("fopen error");
+    return 1;
+  }
+
+ fprint(fp,"Hey my name is Guru!! \n");
+ fclose(fp)
+ return 0;
+}
 
