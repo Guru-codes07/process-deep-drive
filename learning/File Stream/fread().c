14934b9db50d1033afcde4d48898046776b2d5a1
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-21 18:27:42 +0530

    Update fread().c

--- learning/File Stream/fread().c
+++ learning/File Stream/fread().c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main()
+{
+    FILE *fp;
+    char buffer[100];
+
+    fp = fopen("test.txt", "r");
+
+    if (fp == NULL)
+    {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    fread(buffer, sizeof(char), sizeof(buffer) - 1, fp);
+
+    buffer[99] = '\0';
+
+    printf("File content:\n%s\n", buffer);
+
+    fclose(fp);
+
+    return 0;
+}
