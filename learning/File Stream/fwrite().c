60fd27b26df71b6302a03166eb93c93b0802b121
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-22 19:44:00 +0530

    Add fwrite example to write integers to a file
    This program writes an array of integers to a binary file named 'numbers.dat'. It checks for file opening errors and confirms the successful writing of data.

--- learning/File Stream/fwrite().c
+++ learning/File Stream/fwrite().c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main(void)
+{
+    FILE *file;
+    int numbers[] = {10, 20, 30, 40, 50};
+
+    file = fopen("numbers.dat", "wb");
+
+    if (file == NULL)
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+
+ 
+    size_t written = fwrite(numbers, sizeof(int), 5, file);
+
+    if (written == 5)
+    {
+        printf("Data written successfully.\n");
+    }
+    else
+    {
+        printf("Error writing data.\n");
+    }
+
+    fclose(file);
+
+    return 0;
+}
