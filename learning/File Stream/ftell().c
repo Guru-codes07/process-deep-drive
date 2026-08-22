ade25f5842b77d22bfe3ddd5641b90fa2956b371
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-22 19:42:11 +0530

    Implement file reading and position tracking
    Read characters from a file and print their positions.

--- learning/File Stream/ftell().c
+++ learning/File Stream/ftell().c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+int main(void)
+{
+    FILE *file;
+    char ch;
+    long position;
 
+   
+    file = fopen("example.txt", "r");
+
+    if (file == NULL)
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+
+    
+    ch = fgetc(file);
+
+    position = ftell(file);
+
+    printf("Character read: %c\n", ch);
+    printf("Current file position: %ld\n", position);
+
+    ch = fgetc(file);
+
+    position = ftell(file);
+
+    printf("Character read: %c\n", ch);
+    printf("Current file position: %ld\n", position);
+    fclose(file);
+
+    return 0;
+}
