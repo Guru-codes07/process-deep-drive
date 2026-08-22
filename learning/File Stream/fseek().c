1f4ce9d8d491ae68f73b262ef8186dfced891b81
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-22 19:39:24 +0530

    Implement file reading with fseek to get character

--- learning/File Stream/fseek().c
+++ learning/File Stream/fseek().c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+int main(void)
+{
+    FILE *file;
+    char ch;
 
+    /* Open file */
+    file = fopen("example.txt", "r");
+
+    if (file == NULL)
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+
+    /* Move file pointer to the 5th character */
+    fseek(file, 4, SEEK_SET);
+
+    /* Read character */
+    ch = fgetc(file);
+
+    printf("Character at position 5: %c\n", ch);
+
+    /* Close file */
+    fclose(file);
+
+    return 0;
+}
