1749cfbf24d9bb74dcc52f1bfd1c8f1547f9cd02
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-22 19:46:13 +0530

    Update fgets().c

--- learning/File Stream/fgets().c
+++ learning/File Stream/fgets().c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+int main(void)
+{
+    FILE *file;
+    char line[100];
+    file = fopen("hehehe.txt", "r");
 
+    if (file == NULL)
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+    if (fgets(line, sizeof(line), file) != NULL)
+    {
+        printf("Line read from file:\n");
+        printf("%s", line);
+    }
+
+    fclose(file);
+
+    return 0;
+}
