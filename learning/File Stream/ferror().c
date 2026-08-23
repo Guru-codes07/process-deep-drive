9db83e4677a0ded95753a4a6678131424771ffab
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-23 17:01:56 +0530

    Implement file reading with error handling

--- learning/File Stream/ferror().c
+++ learning/File Stream/ferror().c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+int main(void)
+{
+    FILE *file;
+    int ch;
+    file = fopen("huhhh.txt", "r");
 
+    if (file == NULL)
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+    while ((ch = fgetc(file)) != EOF)
+    {
+        putchar(ch);
+    }
+    if (ferror(file))
+    {
+        printf("\nError occurred while reading the file.\n");
+    }
+    else
+    {
+        printf("\nFile read successfully.\n");
+    }
+    fclose(file);
+    return 0;
+}
