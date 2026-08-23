46a7704989bf387ece8782f7553260e6c2008316
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-23 17:00:09 +0530

    Update feof().c

--- learning/File Stream/feof().c
+++ learning/File Stream/feof().c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+int main(void)
+{
+    FILE *file;
+    int ch;
+    file = fopen("example.txt", "r");
+    if (file == NULL)
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+    while (1)
+    {
+        ch = fgetc(file);
 
+        if (feof(file))
+        {
+            break;
+        }
+
+        putchar(ch);
+    }
+ printf("\n\nEnd of file reached.\n");
+ fclose(file);
+ return 0;
+}
