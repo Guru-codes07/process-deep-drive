99f39aa26094fd4a27a0e99144f80cec245b0374
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-24 18:01:08 +0530

    Update fgetpos.c

--- learning/File Stream/fgetpos.c
+++ learning/File Stream/fgetpos.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+int main(void)
+{
+    FILE *file;
+    fpos_t position;
+    int ch;
+    file = fopen("hmmm.txt", "r");
 
+    if (file == NULL)
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+    printf("Characters read: ");
+
+    for (int i = 0; i < 5; i++)
+    {
+        ch = fgetc(file);
+        putchar(ch);
+    }
+    if (fgetpos(file, &position) != 0)
+    {
+        printf("\nUnable to get file position.\n");
+        fclose(file);
+        return 1;
+    }
+
+    printf("\nFile position has been saved.\n");
+    ch = fgetc(file);
+    printf("Next character: %c\n", ch);
+    fclose(file);
+    return 0;
+}
