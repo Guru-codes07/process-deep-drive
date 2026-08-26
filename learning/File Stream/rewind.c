875fec4acceda650e7b539e85c28aa85df312d86
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-26 22:50:28 +0530

    Add file reading example with rewind function
    This program demonstrates reading from a file and using rewind to reset the file pointer.

--- learning/File Stream/rewind.c
+++ learning/File Stream/rewind.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+int main(void)
+{
+    FILE *file;
+    char line[100];
+    file = fopen("bored.txt", "r");
 
+    if (file == NULL)
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+    printf("First read:\n");
+
+    if (fgets(line, sizeof(line), file) != NULL)
+    {
+        printf("%s", line);
+    }
+    rewind(file);
+    printf("\nAfter rewind():\n");
+
+    if (fgets(line, sizeof(line), file) != NULL)
+    {
+        printf("%s", line);
+    }
+    fclose(file);
+
+    return 0;
+}
