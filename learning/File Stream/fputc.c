7fa27aa08cf6244a709e67a4a29c6af4a644b7ff
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-26 22:53:28 +0530

    Implement file writing with fputc in fputc.c
    This program opens a file named 'angry.txt' for writing and writes the characters 'Hello' followed by a newline to it.

--- learning/File Stream/fputc.c
+++ learning/File Stream/fputc.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+int main(void)
+{
+    FILE *file;
+    file = fopen("angry.txt", "w");
 
+    if (file == NULL)
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+    fputc('H', file);
+    fputc('e', file);
+    fputc('l', file);
+    fputc('l', file);
+    fputc('o', file);
+    fputc('\n', file);
+
+    printf("Characters written successfully.\n");
+    fclose(file);
+
+    return 0;
+}
