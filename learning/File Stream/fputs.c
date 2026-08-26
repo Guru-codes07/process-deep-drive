da3a8459efd99832db6f3d4575b6af96afd4cfc5
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-26 22:55:06 +0530

    Add file writing example in fputs.c

--- learning/File Stream/fputs.c
+++ learning/File Stream/fputs.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+    FILE *file;
+    file = fopen("buhahahaha.txt", "w");
+   if (file == NULL)
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+    fputs("Hello World!\n", file);
+    fputs("This is a simple C program.\n", file);
+    fputs("Learning file handling is fun!\n", file);
 
+    printf("Strings written successfully.\n");
+    fclose(file);
+
+    return 0;
+}
