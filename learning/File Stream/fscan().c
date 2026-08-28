2d0cd849644e1d0d6ffe2e23dc2a20b318b50351
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-28 21:51:32 +0530

    Implement file reading and student detail display

--- learning/File Stream/fscan().c
+++ learning/File Stream/fscan().c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+int main(void)
+{
+    FILE *file;
 
+    char name[50];
+    int age;
+    float cgpa;
+    file = fopen("guru.txt", "r");
+
+    if (file == NULL)
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+    fscanf(file, "%49s %d %f", name, &age, &cgpa);
+    printf("Student Details\n");
+    printf("----------------\n");
+    printf("Name : %s\n", name);
+    printf("Age  : %d\n", age);
+    printf("CGPA : %.2f\n", cgpa);
+    fclose(file);
+
+    return 0;
+}
