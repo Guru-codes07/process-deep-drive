281966a040444f98edc61fe8b89e75dd8bbe082d
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-30 20:09:13 +0530

    Implement fork example with process identification

--- learning/Processes/fork.c
+++ learning/Processes/fork.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(void)
+{
+    pid_t pid;
 
+    pid = fork();
+
+    if (pid == 0)
+    {
+        printf("Child process\n");
+    }
+    else if (pid > 0)
+    {
+        printf("Parent process\n");
+    }
+    else
+    {
+        printf("fork() failed\n");
+    }
+
+    return 0;
+}
