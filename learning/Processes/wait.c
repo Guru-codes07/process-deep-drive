a7dd82ee6954a53de708047871e9c7a58393e65c
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-30 20:10:39 +0530

    Implement basic fork and wait in wait.c

--- learning/Processes/wait.c
+++ learning/Processes/wait.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+int main(void)
+{
+    pid_t pid;
 
+    pid = fork();
+
+    if (pid == 0)
+    {
+        printf("Child process\n");
+    }
+    else
+    {
+        wait(NULL);
+        printf("Parent process\n");
+    }
+
+    return 0;
+}
