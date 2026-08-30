212705109c72973b7534dfe30ace441b65a3d378
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-30 20:11:46 +0530

    Update exit.c

--- learning/Processes/exit.c
+++ learning/Processes/exit.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    printf("Program started\n");
 
+    printf("Exiting the program...\n");
+
+    exit(0);
+
+    printf("This will not be printed\n");
+
+    return 0;
+}
