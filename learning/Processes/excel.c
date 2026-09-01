dfc6056b30174aab9e475337120b91568d496164
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-01 20:01:59 +0530

    Update excel.c

--- learning/Processes/excel.c
+++ learning/Processes/excel.c
@@ -1,1 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+    printf("Running ls using execl()\n");
+
+    execl("/bin/ls", "ls", "-l", NULL);
+
+    printf("execl() failed\n");
+
+    return 0;
+}
