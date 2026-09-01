51a68c143acc6d89cb550efb6729e2d3d8b7225f
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-01 20:00:31 +0530

    Update exec.c

--- learning/Processes/exec.c
+++ learning/Processes/exec.c
@@ -1,1 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+    printf("Before exec()\n");
+
+    execl("/bin/ls", "ls", NULL);
+
+    printf("After exec()\n");
+
+    return 0;
+}#include<stdio.h>
