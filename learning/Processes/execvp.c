f9014f2b7e2977a86a591befb58a93ddc0789751
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-01 20:03:00 +0530

    Update execvp.c

--- learning/Processes/execvp.c
+++ learning/Processes/execvp.c
@@ -1,1 +1,15 @@
-#in
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+    char *args[] = {"ls", "-l", NULL};
+
+    printf("Running ls using execvp()\n");
+
+    execvp("ls", args);
+
+    printf("execvp() failed\n");
+
+    return 0;
+}
