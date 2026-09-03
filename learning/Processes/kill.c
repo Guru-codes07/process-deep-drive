ad31e13c73e9b2698938e4cfa4f7c542b143a735
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-03 19:07:37 +0530

    Update kill.c

--- learning/Processes/kill.c
+++ learning/Processes/kill.c
@@ -1,1 +1,14 @@
-#
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+int main(void)
+{
+    printf("Sending SIGTERM to myself...\n");
+
+    kill(getpid(), SIGTERM);
+
+    printf("This will not be printed\n");
+
+    return 0;
+}
