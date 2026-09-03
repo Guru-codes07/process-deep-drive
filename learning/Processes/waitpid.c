7a5ca52c81f207aefc0609786188fa541a9c9304
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-03 19:06:29 +0530

    Update waitpid.c

--- learning/Processes/waitpid.c
+++ learning/Processes/waitpid.c
@@ -1,1 +1,23 @@
-#
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid == 0)
+    {
+        printf("Child process\n");
+        return 0;
+    }
+    else
+    {
+        waitpid(pid, NULL, 0);
+        printf("Parent: child process finished\n");
+    }
+
+    return 0;
+}
