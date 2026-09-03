09dba054972cfba2a538293fdb30840aadd953bb
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-03 19:07:04 +0530

    Update pipe.c

--- learning/Processes/pipe.c
+++ learning/Processes/pipe.c
@@ -1,1 +1,32 @@
-#
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void)
+{
+    int fd[2];
+    char message[] = "Hello from parent";
+    char buffer[50];
+
+    pipe(fd);
+
+    if (fork() == 0)
+    {
+        close(fd[1]);
+
+        read(fd[0], buffer, sizeof(buffer));
+        printf("Child received: %s\n", buffer);
+
+        close(fd[0]);
+    }
+    else
+    {
+        close(fd[0]);
+
+        write(fd[1], message, strlen(message) + 1);
+
+        close(fd[1]);
+    }
+
+    return 0;
+}
