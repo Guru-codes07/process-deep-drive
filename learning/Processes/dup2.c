c1028fa74a28107e42a72eeb46b7760c34523e92
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-04 21:02:52 +0530

    Update dup2.c

--- learning/Processes/dup2.c
+++ learning/Processes/dup2.c
@@ -1,1 +1,17 @@
-i
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void)
+{
+    int fd;
+
+    fd = open("output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    dup2(fd, STDOUT_FILENO);
+    close(fd);
+
+    printf("This goes into output.txt\n");
+
+    return 0;
+}
