a6950d2186329e09963bceda2b50b2c35ac1b38b
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-04 21:04:24 +0530

    Update sigaction.c

--- learning/Processes/sigaction.c
+++ learning/Processes/sigaction.c
@@ -1,1 +1,28 @@
-i
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void handle_signal(int sig)
+{
+    printf("Signal received: %d\n", sig);
+}
+
+int main(void)
+{
+    struct sigaction action;
+
+    action.sa_handler = handle_signal;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+
+    sigaction(SIGINT, &action, NULL);
+
+    printf("Press Ctrl+C to send SIGINT\n");
+
+    while (1)
+    {
+        sleep(1);
+    }
+
+    return 0;
+}
