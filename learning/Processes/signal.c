6ec5f9fe4ebe00b6d9e3a3aed38bfd63ed66258d
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-04 21:01:13 +0530

    Update signal.c

--- learning/Processes/signal.c
+++ learning/Processes/signal.c
@@ -1,1 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void handle_signal(int sig)
+{
+    printf("Signal received: %d\n", sig);
+}
+
+int main(void)
+{
+    signal(SIGINT, handle_signal);
+
+    printf("Press Ctrl+C to send SIGINT\n");
+
+    while (1)
+    {
+        sleep(1);
+    }
+
+    return 0;
+}
