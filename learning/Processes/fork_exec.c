f20dec5ea9e512a916358075311a9ad10aabb1e1
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-06 18:15:12 +0530

    Update fork_exec.c

--- learning/Processes/fork_exec.c
+++ learning/Processes/fork_exec.c
@@ -1,1 +1,29 @@
-#
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid == 0)
+    {
+        char *args[] = {"ls", "-l", NULL};
+
+        printf("Child: running ls\n");
+
+        execvp("ls", args);
+
+        printf("execvp() failed\n");
+    }
+    else
+    {
+        waitpid(pid, NULL, 0);
+
+        printf("Parent: child finished\n");
+    }
+
+    return 0;
+}
