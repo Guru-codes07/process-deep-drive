398f08202b943f6ca03d018a926f9c2650d4bbcd
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-09 15:22:08 +0530

    Update proc_list.c

--- learning/proc/proc_list.c
+++ learning/proc/proc_list.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <ctype.h>
 
+int main(void)
+{
+    DIR *dir;
+    struct dirent *entry;
+
+    dir = opendir("/proc");
+
+    if (dir == NULL)
+    {
+        perror("opendir");
+        return 1;
+    }
+
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if (isdigit(entry->d_name[0]))
+        {
+            printf("PID: %s\n", entry->d_name);
+        }
+    }
+
+    closedir(dir);
+
+    return 0;
+}
