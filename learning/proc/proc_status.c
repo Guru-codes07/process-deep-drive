36496915c3fd3ab163c9dd786ab92fc43edb9908
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-09 15:23:43 +0530

    Add process status viewer for a given PID
    This program prompts the user for a PID and displays the corresponding process status from the /proc filesystem.

--- learning/proc/proc_status.c
+++ learning/proc/proc_status.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(void)
+{
+    int pid;
+    char path[100];
+    char line[256];
+    FILE *file;
+
+    printf("Enter PID: ");
+    scanf("%d", &pid);
+
+    snprintf(path, sizeof(path), "/proc/%d/status", pid);
+
+    file = fopen(path, "r");
+
+    if (file == NULL)
+    {
+        perror("fopen");
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        printf("%s", line);
+    }
+
+    fclose(file);
+
+    return 0;
+}
