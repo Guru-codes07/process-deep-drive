523f8b17e91c07209c76b5d5ad4a6ab5d8478863
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-11 15:05:49 +0530

    Update proc_cpu.c

--- learning/proc/proc_cpu.c
+++ learning/proc/proc_cpu.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int pid;
+    char path[100];
+    FILE *file;
+
+    unsigned long utime;
+    unsigned long stime;
+
+    printf("Enter PID: ");
+    scanf("%d", &pid);
+
+    snprintf(path, sizeof(path), "/proc/%d/stat", pid);
+
+    file = fopen(path, "r");
+
+    if (file == NULL)
+    {
+        perror("fopen");
+        return 1;
+    }
+
+    fscanf(file,
+           "%*d %*s %*c %*d %*d %*d %*d %*d %*u %*u %lu %lu",
+           &utime, &stime);
+
+    printf("User CPU time: %lu\n", utime);
+    printf("Kernel CPU time: %lu\n", stime);
+
+    fclose(file);
+
+    return 0;
+}
