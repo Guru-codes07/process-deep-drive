23dea53ca4821aaa92f91be4012ad0e237f5a3b4
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-11 15:02:29 +0530

    Update proc_cmdline.c

--- learning/proc/proc_cmdline.c
+++ learning/proc/proc_cmdline.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int pid;
+    char path[100];
+    char command[256];
+    FILE *file;
+
+    printf("Enter PID: ");
+    scanf("%d", &pid);
+
+    snprintf(path, sizeof(path), "/proc/%d/cmdline", pid);
+
+    file = fopen(path, "r");
+
+    if (file == NULL)
+    {
+        perror("fopen");
+        return 1;
+    }
+
+    while (fgets(command, sizeof(command), file) != NULL)
+    {
+        printf("Command: %s\n", command);
+    }
+
+    fclose(file);
+
+    return 0;
+}
