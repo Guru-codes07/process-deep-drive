576d3d2f8721dd906cea40bfe95554b8be92d823
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-19 23:07:22 +0530

    Update process_kill.c

--- learning/proc/process_kill.c
+++ learning/proc/process_kill.c
@@ -1,1 +1,22 @@
-#
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main(void)
+{
+    int pid;
+
+    printf("Enter PID to terminate: ");
+    scanf("%d", &pid);
+
+    if (kill(pid, SIGTERM) == 0)
+    {
+        printf("SIGTERM sent to process %d\n", pid);
+    }
+    else
+    {
+        perror("kill");
+    }
+
+    return 0;
+}
