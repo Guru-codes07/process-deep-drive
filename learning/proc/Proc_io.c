1ec47c37be4ad4d5eba688ceffb826edb7e2a1f3
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-19 23:08:33 +0530

    Update Proc_io.c

--- learning/proc/Proc_io.c
+++ learning/proc/Proc_io.c
@@ -1,1 +1,33 @@
-#
+#include <stdio.h>
+
+int main(void)
+{
+    int pid;
+    char path[100];
+    char line[256];
+    FILE *file;
+
+    printf("Enter PID: ");
+    scanf("%d", &pid);
+
+    snprintf(path, sizeof(path), "/proc/%d/io", pid);
+
+    file = fopen(path, "r");
+
+    if (file == NULL)
+    {
+        perror("fopen");
+        return 1;
+    }
+
+    printf("\nI/O information for PID %d:\n", pid);
+
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        printf("%s", line);
+    }
+
+    fclose(file);
+
+    return 0;
+}
