9229ffc0414ac39c384a0ef2cdbe5e67d25ddb9c
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-21 19:57:20 +0530

    Update proc_threads.c

--- learning/proc/proc_threads.c
+++ learning/proc/proc_threads.c
@@ -1,1 +1,39 @@
-#
+#include <stdio.h>
+#include <dirent.h>
+#include <ctype.h>
+
+int main(void)
+{
+    int pid;
+    char path[100];
+
+    DIR *dir;
+    struct dirent *entry;
+
+    printf("Enter PID: ");
+    scanf("%d", &pid);
+
+    snprintf(path, sizeof(path), "/proc/%d/task", pid);
+
+    dir = opendir(path);
+
+    if (dir == NULL)
+    {
+        perror("opendir");
+        return 1;
+    }
+
+    printf("\nThreads of process %d:\n", pid);
+
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if (isdigit(entry->d_name[0]))
+        {
+            printf("Thread ID: %s\n", entry->d_name);
+        }
+    }
+
+    closedir(dir);
+
+    return 0;
+}
