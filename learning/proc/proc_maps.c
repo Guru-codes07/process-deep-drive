e69f327e8bf263a1e0a6bd75845829219dd0c59a
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-22 17:16:13 +0530

    Update proc_maps.c

--- learning/proc/proc_maps.c
+++ learning/proc/proc_maps.c
@@ -1,1 +1,35 @@
-#
+#include <stdio.h>
+
+int main(void)
+{
+    int pid;
+    char path[100];
+    char line[256];
+
+    FILE *file;
+
+    printf("Enter PID: ");
+    scanf("%d", &pid);
+
+    snprintf(path, sizeof(path), "/proc/%d/maps", pid);
+
+    file = fopen(path, "r");
+
+    if (file == NULL)
+    {
+        perror("fopen");
+        return 1;
+    }
+
+    printf("\nMemory mappings for PID %d:\n", pid);
+    printf("--------------------------------\n");
+
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        printf("%s", line);
+    }
+
+    fclose(file);
+
+    return 0;
+}
