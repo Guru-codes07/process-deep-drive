f7f840d849dd61c988c3f2beb62f704b1823c6d1
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-22 17:17:45 +0530

    Update proc_system.c

--- learning/proc/proc_system.c
+++ learning/proc/proc_system.c
@@ -1,1 +1,36 @@
-#
+#include <stdio.h>
+
+void show_file(const char *path)
+{
+    FILE *file;
+    char line[256];
+
+    file = fopen(path, "r");
+
+    if (file == NULL)
+    {
+        perror(path);
+        return;
+    }
+
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        printf("%s", line);
+    }
+
+    fclose(file);
+}
+
+int main(void)
+{
+    printf(" MEMORY (/proc/meminfo) \n");
+    show_file("/proc/meminfo");
+
+    printf("\n LOAD (/proc/loadavg) \n");
+    show_file("/proc/loadavg");
+
+    printf("\n CPU (/proc/stat) \n");
+    show_file("/proc/stat");
+
+    return 0;
+}
