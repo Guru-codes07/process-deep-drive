09306f9bde72f2600cf8431ef9273c033ed0e4a3
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-26 19:40:39 +0530

    Update proc_stat.c

--- learning/proc/proc_stat.c
+++ learning/proc/proc_stat.c
@@ -1,1 +1,48 @@
-#
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *file;
+
+    unsigned long user;
+    unsigned long nice;
+    unsigned long system;
+    unsigned long idle;
+    unsigned long iowait;
+    unsigned long irq;
+    unsigned long softirq;
+    unsigned long steal;
+
+    file = fopen("/proc/stat", "r");
+
+    if (file == NULL)
+    {
+        perror("fopen");
+        return 1;
+    }
+
+    fscanf(file, "cpu %lu %lu %lu %lu %lu %lu %lu %lu",
+           &user,
+           &nice,
+           &system,
+           &idle,
+           &iowait,
+           &irq,
+           &softirq,
+           &steal);
+
+    fclose(file);
+
+    printf("System CPU Statistics:\n");
+
+    printf("User:        %lu\n", user);
+    printf("Nice:        %lu\n", nice);
+    printf("System:      %lu\n", system);
+    printf("Idle:        %lu\n", idle);
+    printf("I/O Wait:    %lu\n", iowait);
+    printf("IRQ:         %lu\n", irq);
+    printf("Soft IRQ:    %lu\n", softirq);
+    printf("Steal:       %lu\n", steal);
+
+    return 0;
+}
