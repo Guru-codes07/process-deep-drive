5f50df20469ce808e9e8110ec16d40b8f3b189e0
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-26 19:40:00 +0530

    Update proc_meminfo.c

--- learning/proc/proc_meminfo.c
+++ learning/proc/proc_meminfo.c
@@ -1,1 +1,42 @@
-#
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    FILE *file;
+    char line[256];
+
+    unsigned long total = 0;
+    unsigned long free_mem = 0;
+    unsigned long available = 0;
+
+    file = fopen("/proc/meminfo", "r");
+
+    if (file == NULL)
+    {
+        perror("fopen");
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        if (sscanf(line, "MemTotal: %lu kB", &total) == 1)
+            continue;
+
+        if (sscanf(line, "MemFree: %lu kB", &free_mem) == 1)
+            continue;
+
+        if (sscanf(line, "MemAvailable: %lu kB", &available) == 1)
+            continue;
+    }
+
+    fclose(file);
+
+    printf("Memory Information:\n");
+
+    printf("Total RAM:     %lu kB\n", total);
+    printf("Free RAM:      %lu kB\n", free_mem);
+    printf("Available RAM: %lu kB\n", available);
+
+    return 0;
+}
