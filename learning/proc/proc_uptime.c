99e800350b7c1154b2f4c53815c782ab3192f3e9
Guru Prassad Mishra <2020gurup@gmail.com>
2026-09-26 19:38:50 +0530

    Update proc_uptime.c

--- learning/proc/proc_uptime.c
+++ learning/proc/proc_uptime.c
@@ -1,1 +1,24 @@
-#
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *file;
+    double uptime;
+    double idle_time;
+
+    file = fopen("/proc/uptime", "r");
+
+    if (file == NULL)
+    {
+        perror("fopen");
+        return 1;
+    }
+
+    fscanf(file, "%lf %lf", &uptime, &idle_time);
+
+    fclose(file);
+
+    printf("System uptime: %.2f seconds\n", uptime);
+
+    return 0;
+}
